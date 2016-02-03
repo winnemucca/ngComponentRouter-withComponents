5251634ad8ea78a2da753ffa7f01c86dbd6e9aa0
winnemucca <skauyedauty@gmail.com>
2016-02-03 13:25:23 -0700

    new router with components

---
+++ README.md
@@ -0,0 +1,8 @@
+# Angular 1 Component Router Demo
+
+This application showcases the new component router that is available
+for Angular 1.5
+
+## Running the App
+
+**Install the dependencies**
---
+++ app/heroes.js
@@ -0,0 +1,104 @@
+"use strict";
+
+angular.module('heroes', [])
+  .service('heroService', HeroService)
+
+  .component('heroes', {
+    template: '<h2>Heroes</h2><ng-outlet></ng-outlet>',
+    $routeConfig: [
+      {path: '/',    name: 'HeroList',   component: 'heroList', useAsDefault: true},
+      {path: '/:id', name: 'HeroDetail', component: 'heroDetail'}
+    ]
+  })
+
+  .component('heroList', {
+    template:
+      '<div ng-repeat="hero in $ctrl.heroes">\n' +
+        '<a ng-link="[\'HeroDetail\', {id: hero.id}]">{{hero.name}}</a>\n' +
+      '</div>',
+    controller: HeroListComponent
+  })
+
+  .component('heroDetail', {
+    template:
+      '<div ng-if="$ctrl.hero">\n' +
+      '  <h3>"{{$ctrl.hero.name}}"</h3>\n' +
+      '  <div>\n' +
+      '    <label>Id: </label>{{$ctrl.hero.id}}</div>\n' +
+      '  <div>\n' +
+      '    <label>Name: </label>\n' +
+      '    <input ng-model="$ctrl.hero.name" placeholder="name"/>\n' +
+      '  </div>\n' +
+      '  <button ng-click="$ctrl.gotoHeroes()">Back</button>\n' +
+      '</div>\n',
+    controller: HeroDetailComponent
+  });
+
+
+function HeroService($q) {
+  var heroesPromise = $q.when([
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' }
+  ]);
+
+  this.getHeroes = function() {
+    return heroesPromise;
+  };
+
+  this.getHero = function(id) {
+    return heroesPromise.then(function(heroes) {
+      for(var i=0; i<heroes.length; i++) {
+        if ( heroes[i].id == id) return heroes[i];
+      }
+    });
+  };
+}
+
+function HeroListComponent(heroService) {
+  var _selectedId = null;
+  var _router;
+  var ctrl = this;
+
+  this.$routerOnActivate = function(router, next) {
+    // Get hold of the nearest router
+    _router = router;
+
+    // Load up the heroes for this view
+    heroService.getHeroes().then(function(heroes) {
+      ctrl.heroes = heroes;
+      _selectedId = next.params.id;
+    });
+  };
+
+  this.isSelected = function(hero) {
+    return (hero.id == _selectedId);
+  };
+
+  this.onSelect = function(hero) {
+    _router.navigate(['HeroDetail', { id: hero.id }]);
+  };
+}
+
+function HeroDetailComponent(heroService, $router) {
+  var ctrl = this;
+  var _router;
+
+  this.$routerOnActivate = function(router, next) {
+    // Get hold of the nearest router
+    _router = router;
+    // Get the hero identified by the route parameter
+    var id = next.params.id;
+    heroService.getHero(id).then(function(hero) {
+      ctrl.hero = hero;
+    });
+  };
+
+  this.gotoHeroes = function() {
+    var heroId = this.hero && this.hero.id;
+    _router.navigate(['HeroList', {id: heroId}]);
+  };
+}
